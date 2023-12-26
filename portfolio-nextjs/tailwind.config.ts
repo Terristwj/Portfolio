372d6f4b58e787e788660854c7c6b7a9b1e2bfd2
Terris <27739101+Terristwj@users.noreply.github.com>
2023-12-26 16:47:39 +0800

    Created index page and setup

---
+++ portfolio-nextjs/tailwind.config.ts
@@ -0,0 +1,18 @@
+import type { Config } from "tailwindcss";
+
+const config: Config = {
+    content: [
+        "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+        "./components/**/*.{js,ts,jsx,tsx,mdx}",
+        "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    ],
+    darkMode: "class",
+    theme: {
+        extend: {},
+    },
+    plugins: [
+        require("@tailwindcss/typography"),
+        require("@tailwindcss/forms"),
+    ],
+};
+export default config;
