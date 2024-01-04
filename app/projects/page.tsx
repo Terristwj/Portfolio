8f4f012f68cc1e0cefb1ae3e3be9c5d7c6a67adb
Terris <27739101+Terristwj@users.noreply.github.com>
2024-01-04 23:46:51 +0800

    Guestbook test 4

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -98,8 +98,9 @@ export default function Projects() {
                         {projectArray.map((project, index) => (
                             <motion.article
                                 key={index}
-                                initial={{ opacity: 0, y: -10 }}
+                                initial={{ opacity: 0.5, y: -10 }}
                                 animate={{ opacity: 1, y: 0 }}
+                                exit={{ opacity: 0 }}
                                 transition={{
                                     duration: 0.5,
                                     delay: 0.1 * index,
