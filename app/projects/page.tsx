b6c8b4224c966dab5ae60cc23a5e8b19a4a0cae8
Terris <27739101+Terristwj@users.noreply.github.com>
2024-01-04 23:41:26 +0800

    Guestbook test 3

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -98,11 +98,8 @@ export default function Projects() {
                         {projectArray.map((project, index) => (
                             <motion.article
                                 key={index}
-                                initial={{ opacity: 0, y: -10 }}
-                                animate={{ opacity: 1, y: 0 }}
-                                exit={{ opacity: 0 }}
                                 transition={{
-                                    duration: 0.8,
+                                    duration: 0.5,
                                     delay: 0.1 * index,
                                 }}
                                 className="border rounded-md
