823b89377da81400a7b6650ef8de812f4ecc1ff7
Terris <27739101+Terristwj@users.noreply.github.com>
2024-01-06 21:06:59 +0800

    Added realtime for Guestbook

--- app/guestbook/page.tsx
+++ app/guestbook/page.tsx
@@ -1,14 +1,20 @@
 import { getEntries } from "../api/dbAction";
 
-import Form from "../components/Guestbook/Form";
 import PageAnimate from "../components/wrappers/PageAnimate";
 import PageDefault from "../components/wrappers/PageDefault";
 
+import Form from "../components/Guestbook/Form";
+import EntryResults from "../components/Guestbook/EntryResults";
+
 // Resets cache data after 5 sec
-export const revalidate = 5;
+// export const revalidate = 5;
+
+// Settings
+const numEntries = 10;
+const reloadInterval = 5000;
 
 export default async function Guestbook() {
-    let data = await getEntries(10);
+    let data = await getEntries(numEntries);
 
     return (
         <PageAnimate>
@@ -18,21 +24,10 @@ export default async function Guestbook() {
                     <Form />
 
                     {/* Display Results */}
-                    <div className="flex flex-col space-y-2 font-medium">
-                        {data.map((entry) => (
-                            <div
-                                key={entry.id}
-                                className="w-full break-words leading-5"
-                            >
-                                <span className="text-teal-600 dark:text-teal-500">
-                                    {entry.username}:
-                                </span>{" "}
-                                <span className="montserrat">
-                                    {entry.message}
-                                </span>
-                            </div>
-                        ))}
-                    </div>
+                    <EntryResults
+                        entries={data}
+                        reloadInterval={reloadInterval}
+                    />
                 </div>
             </PageDefault>
         </PageAnimate>
