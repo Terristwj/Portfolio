f58ff486fa397f4bdaf71d20ea71dd09158fb911
Terris <27739101+Terristwj@users.noreply.github.com>
2024-08-14 01:43:17 +0800

    Guestbook API - Test 4

--- app/api/guestbook/route.ts
+++ app/api/guestbook/route.ts
@@ -13,7 +13,7 @@ import isProd from "@/app/api/guestbook/constants";
 
 // Route: /api/guestbook
 // - Get all messages from tmp/messages.json in JSON format.
-export async function GET(): Promise<NextResponse<IMessage[]>> {
+export async function GET(): Promise<NextResponse<IMessage[] | string>> {
     // Retrieve temporary directory URL
     const directory: string = tmpdir();
     const tempJsonUrl: string = `${directory}/messages.json`;
@@ -32,12 +32,14 @@ export async function GET(): Promise<NextResponse<IMessage[]>> {
                 }
             }
         );
+
+        // Read the temporary JSON file
+        const data: string = fs.readFileSync(tempJsonUrl).toString();
+        const dbMessages: Array<IMessage> = JSON.parse(data);
+
+        // Return the messages in JSON format
+        return NextResponse.json(dbMessages);
     }
 
-    // Read the temporary JSON file
-    const data: string = fs.readFileSync(tempJsonUrl).toString();
-    const dbMessages: Array<IMessage> = JSON.parse(data);
-
-    // Return the messages in JSON format
-    return NextResponse.json(dbMessages);
+    return NextResponse.json("Error");
 }
