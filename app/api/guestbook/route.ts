9e91f2d18d02b486bed94764f67861e3249eaa75
Terris <27739101+Terristwj@users.noreply.github.com>
2024-08-14 01:52:30 +0800

    Guestbook API - Test 5

--- app/api/guestbook/route.ts
+++ app/api/guestbook/route.ts
@@ -2,12 +2,29 @@
 
 import { NextResponse } from "next/server";
 
-import { getAllMessages } from "@/app/api/guestbook/MessageActions";
+// File System - Read/Write JSON
+import { tmpdir } from "os";
+import fs from "fs";
+
+// My Messages
 import IMessage from "@/app/guestbook/components/MessageInterface";
 
 // Route: /api/guestbook
 // - Get all messages in JSON format.
-export async function GET() {
-    const dbMessages: IMessage[] = await getAllMessages("DESC");
+export async function GET(): Promise<NextResponse<IMessage[]>> {
+    // Retrieve temporary directory URL
+    const directory: string = tmpdir();
+    const tempJsonUrl: string = `${directory}/messages.json`;
+
+    // Retrieve temporary JSON file data
+    let dbMessages: Array<IMessage> = [];
+    try {
+        const data: string = fs.readFileSync(tempJsonUrl).toString();
+        dbMessages = JSON.parse(data);
+    } catch (error: any) {
+        // JSON file does not exist yet
+        console.log("Error:", error);
+    }
+
     return NextResponse.json(dbMessages);
 }
